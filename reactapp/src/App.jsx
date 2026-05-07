ca56f15bd7554b8d634002bcd89b6f9560d0ea1d
Chaos <3855vishaljangir8thb@gmail.com>
2026-05-07 09:25:41 +0530

    commit

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,119 +1,36 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Sample from './component/Sample'
 import Navbar from './component/Navbar'
+import HomePage from './component/HomePage'
+import Contact from './component/Contact'
+import { Route, Routes } from 'react-router-dom'
+import Profile from './component/Profile'
+import { UserContext } from './UserContext'
+import Search from './component/Search'
+import Product from './component/Product'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menu, setMenu] = useState("Sports")
+  const [abc, setAbc] = useState("Entertainment")
+  const name="Bob"
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <Navbar/>
-        <Sample/>
 
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-       
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <UserContext.Provider value={name}>
+          <Routes>
+            <Route path="/" element={<><HomePage menu={menu} /></>} >
+            
+            </Route>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/sample" element={<><Sample /></>} />
+            <Route path="/profile/:username" element={<><Profile/></>} />
+            <Route path="/search" element={<><Search/></>} />
+            <Route path="/product" element={<><Product/></>} />
+        </Routes>
+      </UserContext.Provider>
+      
     </>
   )
 }
